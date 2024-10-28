bfa336eb80c49a0e05cfd49408c20387202fbf4f
lenobles <aboubacarlenobles@gmail.com>
2024-10-28 14:14:28 +0100

    oui
    oui jai fait

---
+++ script - Kopie.js
@@ -0,0 +1,7 @@
+// Ajoutez une logique pour afficher les détails des recettes ici
+document.querySelectorAll('.details').forEach(link => {
+    link.addEventListener('click', (event) => {
+        event.preventDefault();
+        alert('Détails de la recette non implémentés.');
+    });
+});
